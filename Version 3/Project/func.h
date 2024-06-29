2db2c2f268efcbd71c7d69496622e113662e4e3b
Cebrail Kutlar <cebolicebo917@gmail.com>
2024-06-29 22:23:08 +0300

    Add files via upload

---
+++ Version 3/Project/func.h
@@ -0,0 +1,46 @@
+#define FUNC_H
+#ifdef FUNC_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include "source.h"
+
+int length;
+
+float max(float y, float s)
+{ if (y < s) return s; else return y; }
+float min(float y, float s)
+{ if (s < y) return (s - s); else return y - y; }
+float Sigmoid(float s1, float s2)
+{ return 1 / (1 + exp(-(s1 * s2) + ((s1 + s2) / 2))); }
+float ReLU(float s)
+{ return max(0,s); }
+float Formul1(float s1, float s2)
+{ return sqrt((pow(ReLU(s1), s2) + pow(ReLU(s2), s1))); }
+float Formul2(float s)
+{ return max(0,s) + 0.1 * min(0,s); }
+float Formul3(float s1, float s2)
+{ return (exp(s1) / exp(s2)) * exp(s1 + s2); }
+float Formul4(float s1, float s2)
+{ return (s1 * s2) + (s1 + s2); }
+float Formul5(float s1, float s2)
+{ return 1 / (1 + sqrt(pow(s1, s2))); }
+float Formul6(float s1, float s2)
+{ return (Formul2(s1) + Formul2(s2)) / 2; }
+bool _xor(bool s1, bool s2)
+{ return (s1 != s2); }
+void hamming(bool data[length][length], bool cracked_data[length][length]){
+	bool ram[length][length];
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{ 
+			ram[i][j] = _xor( _xor(data[i][j], data[i][j]) , _xor(data[i][j], data[i][j]));
+			if (ram[i][j] != cracked_data[i][j]) 
+			{ printf("\n ( " COLOR_CYAN "%1d" COLOR_RESET " , " COLOR_CYAN "%1d" COLOR_RESET " ) " COLOR_MAGENTA "Address Error" COLOR_RESET " ", i, j); }
+			else { printf(COLOR_GREEN "\n Nothing Wrong ? " COLOR_RESET); }
+		}
+	}
+}
+#endif
---
+++ Version 3/Project/source.h
@@ -0,0 +1,105 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m"
+
+void box(int x, int y)
+{
+	for(int i = 0; i < x; i++)
+	{
+		printf("\n ");
+		for(int j = 0; j < y; j++)
+		{
+			printf("#");
+		}
+	}
+	// printf(" Area : %d , Height : %d", (x + y)*2, (x * y));
+}
+
+void triangle(int x, int y)
+{
+	for(int i = 0; i < x; i++)
+	{
+		printf("#");
+		for(int j = i; j < y; j++)
+		{
+			printf("#");
+		}
+	}
+}
+
+float operator(float s1, float s2, char *_operator)
+{
+	if(strcmp(_operator, "+")) { return s1 + s2; }
+	else if (strcmp(_operator, "-"))
+       	{
+		if (s1 < s2) return s2 - s1;
+		else if (s2 < s1) return s1 - s2;
+		else return 0;
+       	}
+	else if (strcmp(_operator, "*")) return s1 * s2;
+	else if (strcmp(_operator, "/"))
+	{
+		if (s1 < s2) return s2 / s1;
+		else if (s2 < s1) return s1 / s2;
+		else return 0;
+	}
+	else if (strcmp(_operator, "POW")) return pow(s1, s2);
+	else if (strcmp(_operator, "SQRT(1)")) return sqrt(s1);
+	else if (strcmp(_operator, "SQRT(2)")) return sqrt(s2);
+	else if (strcmp(_operator, ">"))
+	{
+		if (s1 > s2) return true;
+		else return false;
+	}
+	else if (strcmp(_operator, "<"))
+	{
+		if (s1 < s2) return true;
+		else return false;
+	}
+	else return 0;
+}
+
+void File(int _operator, char *filePath)
+{
+	FILE *f;
+	if (_operator == 1)
+	{
+		f = fopen(filePath, "r");
+		char *num;
+		fscanf(f, "%s", &num);
+		printf(" Value : %s", num);
+		fclose(f);
+	}
+	else if (_operator == 2)
+	{
+		f = fopen(filePath, "w");
+		char *line;
+		fscanf(f, "%s", &line);
+		fprintf(f, "%s", line);
+		fclose(f);
+	}
+	else if (_operator == 3)
+	{
+		char *name;
+		scanf(" File Name : %s\n", &name);
+		f = fopen(name, "w");
+		fclose(f);
+	}
+	else
+	{
+		printf("\n");
+	}
+}
+
+#endif
